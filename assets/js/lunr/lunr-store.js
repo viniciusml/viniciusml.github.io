014f7232f28e776ba6dabfbd388a612a7aee048d
Vinicius Leal <viniciusmoreiraleal@gmail.com>
2020-04-26 19:04:28 +0200

    build with jekyll production serve

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -2,30 +2,30 @@ var store = [{
         "title": "Apollo Explorer",
         "excerpt":"The app was published as the result of the final project for the Apple Developer Academy course. Our group of 5 students had 3 months to come up with an idea, research about the topic, come up with a design and develop it. Apollo explorer helps users to inspired by...","categories": [],
         "tags": ["CoreML","Vapor","SDWebImage","AVKit"],
-        "url": "http://localhost:4000/portfolio/AAA-02-apollo-explorer-identity/",
-        "teaser": "http://localhost:4000/assets/images/apollo-present.png"
+        "url": "https://vinileal.com/portfolio/AAA-02-apollo-explorer-identity/",
+        "teaser": "https://vinileal.com/assets/images/apollo-present.png"
       },{
         "title": "Blossom Journey",
         "excerpt":"A meat free diet can make a considerable difference in waste production, gas emissions and water usage. Blossom Journey lets you see that in an elegant format. The sharing of your lifestyle’s outcomes might be useful to engage the vegan and vegetarian community and also for people who follow a...","categories": [],
         "tags": ["Programmatic UI","UIKit"],
-        "url": "http://localhost:4000/portfolio/AAA-03-blossom-journey-identity/",
-        "teaser": "http://localhost:4000/assets/images/blossom-present.png"
+        "url": "https://vinileal.com/portfolio/AAA-03-blossom-journey-identity/",
+        "teaser": "https://vinileal.com/assets/images/blossom-present.png"
       },{
         "title": "Sammy On Ice",
         "excerpt":"Accepted for WWDC 2019 Scholarship. This is a fun Playground Book, with a ‘children’s book’ feel. It shows a baby emperor penguin telling a story. The book talks a little about their society’s complexity, their resilience and family values. Meet Sammy The main character is Sammy, a baby emperor penguin....","categories": [],
         "tags": ["SpriteKit","CoreMotion","Adobe Illustrator","PlaygroundBooks"],
-        "url": "http://localhost:4000/portfolio/AAA-04-sammy-on-ice-identity/",
-        "teaser": "http://localhost:4000/assets/images/sammy-present.png"
+        "url": "https://vinileal.com/portfolio/AAA-04-sammy-on-ice-identity/",
+        "teaser": "https://vinileal.com/assets/images/sammy-present.png"
       },{
         "title": "iRecorder",
         "excerpt":"iRecorder is an app example that utilizes core animation in conjunction with pan gesture recognizers to provide a good user experience for recording voice memos. The UI is 100% programmatic, and the audio recording and playing is managed by AVFoundation. The voice animation is obtained by utilizing CAReplicatorLayer and CABasicAnimation...","categories": [],
         "tags": ["AVFoundation","CALayer","UIGestureRecognizer"],
-        "url": "http://localhost:4000/portfolio/CCC-01-i-recorder-identity/",
-        "teaser": "http://localhost:4000/assets/images/i-recorder-present.png"
+        "url": "https://vinileal.com/portfolio/CCC-01-i-recorder-identity/",
+        "teaser": "https://vinileal.com/assets/images/i-recorder-present.png"
       },{
         "title": "Swift Initializers",
         "excerpt":"Have you ever stopped to think about why you do what you do? I think it happens with all of us, at different levels, of course. In iOS development it can happen too. You write some code, just for the sake of writing, and then ask yourself: why? That’s what...","categories": ["Initialization","Basics"],
         "tags": ["intitializers","basics"],
-        "url": "http://localhost:4000/initialization/basics/swift-initializers/",
-        "teaser": null
+        "url": "https://vinileal.com/initialization/basics/swift-initializers/",
+        "teaser": "https://vinileal.com/assets/images/initializers-header.png"
       }]
