8f1153356967f26cb174470431044f30c12b6ced
Vinicius Leal <viniciusmoreiraleal@gmail.com>
2022-08-23 21:18:46 +0100

    Add tech debt article (#1)

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,91 @@
+var store = [{
+        "title": "Apollo Explorer",
+        "excerpt":"The app was published as the result of the final project for the Apple Developer Academy course. Our group of 5 students had 3 months to come up with an idea, research about the topic, come up with a design and develop it. Apollo explorer helps users to inspired by...","categories": [],
+        "tags": ["CoreML","Vapor","SDWebImage","AVKit"],
+        "url": "https://vinileal.com/portfolio/AAA-02-apollo-explorer-identity/",
+        "teaser": "https://vinileal.com/assets/images/apollo-present.png"
+      },{
+        "title": "Blossom Journey",
+        "excerpt":"A meat free diet can make a considerable difference in waste production, gas emissions and water usage. Blossom Journey lets you see that in an elegant format. The sharing of your lifestyle’s outcomes might be useful to engage the vegan and vegetarian community and also for people who follow a...","categories": [],
+        "tags": ["Programmatic UI","UIKit"],
+        "url": "https://vinileal.com/portfolio/AAA-03-blossom-journey-identity/",
+        "teaser": "https://vinileal.com/assets/images/blossom-present.png"
+      },{
+        "title": "Sammy On Ice",
+        "excerpt":"Accepted for WWDC 2019 Scholarship. This is a fun Playground Book, with a ‘children’s book’ feel. It shows a baby emperor penguin telling a story. The book talks a little about their society’s complexity, their resilience and family values. Meet Sammy The main character is Sammy, a baby emperor penguin....","categories": [],
+        "tags": ["SpriteKit","CoreMotion","Adobe Illustrator","PlaygroundBooks"],
+        "url": "https://vinileal.com/portfolio/AAA-04-sammy-on-ice-identity/",
+        "teaser": "https://vinileal.com/assets/images/sammy-present.png"
+      },{
+        "title": "iRecorder",
+        "excerpt":"iRecorder is an app example that utilizes core animation in conjunction with pan gesture recognizers to provide a good user experience for recording voice memos. The UI is 100% programmatic, and the audio recording and playing is managed by AVFoundation. The voice animation is obtained by utilizing CAReplicatorLayer and CABasicAnimation...","categories": [],
+        "tags": ["AVFoundation","CALayer","UIGestureRecognizer"],
+        "url": "https://vinileal.com/portfolio/CCC-01-i-recorder-identity/",
+        "teaser": "https://vinileal.com/assets/images/i-recorder-present.png"
+      },{
+        "title": "Swift Initializers",
+        "excerpt":"Have you ever stopped to think about why you do what you do? I think it happens with all of us, at different levels, of course. In iOS development it can happen too. You write some code, just for the sake of writing, and then ask yourself: why? That’s what...","categories": ["Initialization","Basics"],
+        "tags": ["intitializers","basics"],
+        "url": "https://vinileal.com/initialization/basics/swift-initializers/",
+        "teaser": "https://vinileal.com/assets/images/initializers-header.png"
+      },{
+        "title": "Code Reviews",
+        "excerpt":"How can you improve your team’s productivity with code reviews? There are quite a few things in life that you only truly see the importance after experiencing, and code reviews are definitely one of those. By talking to my colleagues who apply it frequently in their workflow I could see...","categories": ["Productivity","Basics"],
+        "tags": ["productivity","basics"],
+        "url": "https://vinileal.com/productivity/basics/code-reviews/",
+        "teaser": "https://vinileal.com/assets/images/code-reviews-header.png"
+      },{
+        "title": "Test Driving UIPageViewController implementation",
+        "excerpt":"A UIPageViewController is a very popular element in iOS Projects. It’s commonly seen in onboarding screens, but it has a ton of other uses. Me and my friend Mauro Marques are using one for the onboarding screen of our app Hira, and while I was developing the beautiful screens desgined...","categories": ["Tutorial","TDD"],
+        "tags": ["unit tests","tdd","UIPageViewController","UIKit"],
+        "url": "https://vinileal.com/tutorial/tdd/tdd-uipageviewcontroller/",
+        "teaser": "https://vinileal.com/assets/images/uipagecontroller-header.png"
+      },{
+        "title": "Microinteractions in SwiftUI - Menu Button Animation",
+        "excerpt":"Microinteractions have become increasingly important in a world with a dizzying number of digital platforms and an ocean of content. While microinteractions used to be considered an interesting resource in the early days of digital design, in today’s hypercompetitive digital space they have become a crucial element in the overall...","categories": ["Tutorial","SwiftUI","Microinteraction","Animation"],
+        "tags": ["SwiftUI"],
+        "url": "https://vinileal.com/tutorial/swiftui/microinteraction/animation/menu-button-swiftui/",
+        "teaser": "https://vinileal.com/assets/images/microinteractions-swiftui-header.png"
+      },{
+        "title": "Continuous Integration (CI) - An Introduction",
+        "excerpt":"A software development team can benefit from many methodologies that will save them time and repetitive work. As an example, there is continuous integration, continuous delivery and continuous deployment. Methods and techniques like these represent ways to accelerate the delivery of new versions of the application, and are aligned with...","categories": ["Agile"],
+        "tags": ["continuous integration","CI","Agile"],
+        "url": "https://vinileal.com/agile/continuous-integration-intro/",
+        "teaser": "https://vinileal.com/assets/images/ci-header.png"
+      },{
+        "title": "Design Patterns in Swift - Template Method",
+        "excerpt":"When we think about the software development routine, we can quickly identify that many times, the solution to a specific problem has identical characteristics, if not equal to that found in a previously developed project but the solution and the problem had not been documented. So, you are facing a...","categories": ["Design Patterns"],
+        "tags": ["template method","design patterns"],
+        "url": "https://vinileal.com/design%20patterns/design-patterns-swift-template-method/",
+        "teaser": "https://vinileal.com/assets/images/design-patterns-template-method-header.png"
+      },{
+        "title": "Common Git Workflows",
+        "excerpt":"This article aims to expose a few of the approaches on how to work using workflows with git. If you already work with git as the main version control tool, you may have seen several approaches on how to use and control branches in a production or personal setting. And...","categories": ["Git"],
+        "tags": ["git workflows","git"],
+        "url": "https://vinileal.com/git/common-git-workflows/",
+        "teaser": "https://vinileal.com/assets/images/git-flow-header.png"
+      },{
+        "title": "What is Open Sorce and How to Start Contributing",
+        "excerpt":"The world today is more democratic for those who want or need to use computers, more precisely those who need to make use of computer programs. But this was not always the case, and in part what made access to the computer world something a little simpler or less expensive...","categories": ["Open Source"],
+        "tags": ["open source","pull request","git","hacktoberfest"],
+        "url": "https://vinileal.com/open%20source/open-source-how-to-contribute/",
+        "teaser": "https://images.unsplash.com/photo-1555949963-ff9fe0c870eb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=3450&q=80"
+      },{
+        "title": "Improving the Testability of Apple Pay With Dependency Injection",
+        "excerpt":"We all know the importance of writing unit tests. But we also have seen many people who say that they take time to write, some things do not need to be tested, and all sorts of things that divide developer’s opinions as much as the endless debate ‘storyboards vs. programmatic...","categories": ["Tutorial","Unit Test"],
+        "tags": ["unit tests","tdd","Apple Pay","PassKit"],
+        "url": "https://vinileal.com/tutorial/unit%20test/unit-test-apple-pay/",
+        "teaser": "https://vinileal.com/assets/images/unit-test-apple-pay-header.png"
+      },{
+        "title": "Design Patterns in Swift - Decorator",
+        "excerpt":"As I mentioned in the first article of this series, Design patterns are solutions to problems that someone once had and solved by applying a model that has been documented and that you can adapt entirely or according to the need for your solution. The original idea of Design Patterns...","categories": ["Design Patterns"],
+        "tags": ["decorator","design patterns"],
+        "url": "https://vinileal.com/design%20patterns/design-patterns-swift-decorator/",
+        "teaser": "https://vinileal.com/assets/images/design-patterns-decorator-header.png"
+      },{
+        "title": "Tech Debt",
+        "excerpt":"It is a well known fact that time is a driving force in multiple industries. For software development, it is a constant challenge, to be able to reconcile the fulfilment of stipulated deadlines with other needs of the project (or product). In this battle against time, often we are faced...","categories": ["Agile"],
+        "tags": ["tech debt"],
+        "url": "https://vinileal.com/agile/tech-debt/",
+        "teaser": null
+      }]
